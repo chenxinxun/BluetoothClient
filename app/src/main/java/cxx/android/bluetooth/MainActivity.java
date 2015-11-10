20326a5eaeb0d8dec34ac644ff2a8a7debf53f68
chenxinxun <chen_303507@163.com>
2015-11-10 18:48:06 +0800

    将PSE的联系人获取，并添加到PCE的通讯录中

--- app/src/main/java/cxx/android/bluetooth/MainActivity.java
+++ app/src/main/java/cxx/android/bluetooth/MainActivity.java
@@ -3,22 +3,24 @@ package cxx.android.bluetooth;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import  cxx.bluetooth.client.pbap.BluetoothPbapClient;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Set;
-import java.util.UUID;
 
-import javax.obex.ClientSession;
 import javax.obex.ObexTransport;
-import android.util.Log;
+
+import cxx.android.vcard.VCardEntry;
+import cxx.android.vcard.VCardEntryCommitter;
+import cxx.bluetooth.client.pbap.BluetoothPbapClient;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "cxx.bluetooth.client";
     private BluetoothAdapter mBluetoothAdapter;
@@ -26,6 +28,25 @@ public class MainActivity extends AppCompatActivity {
     private  ObexTransport mTransport;
     private static final String PBAP_UUID =
             "0000112f-0000-1000-8000-00805f9b34fb";
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case BluetoothPbapClient.EVENT_PULL_PHONE_BOOK_DONE:
+                    ArrayList<VCardEntry> list = (ArrayList<VCardEntry>)msg.obj;
+                    VCardEntryCommitter committer = new VCardEntryCommitter(getContentResolver());
+                    for (int i =0; i < list.size(); i++){
+                        committer.onEntryCreated(list.get(i));
+                        Log.d(TAG, "a" + list.get(i).getDisplayName());
+                    }
+
+                    break;
+                default:
+
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,11 +70,9 @@ public class MainActivity extends AppCompatActivity {
         for (BluetoothDevice device: devices) {
             try {
                // mSocket = device.createRfcommSocketToServiceRecord(UUID.fromString(PBAP_UUID));
-                BluetoothPbapClient client = new BluetoothPbapClient(device,new Handler());
+                BluetoothPbapClient client = new BluetoothPbapClient(device,handler);
                 client.connect();
-               boolean flg = client.pullVcardListing(BluetoothPbapClient.PB_PATH);
-                Log.d(TAG, "Handler: msg: " + flg);
-                //    mSessionHandler.obtainMessage(RFCOMM_CONNECTED, transport).sendToTarget();
+                boolean flg =   client.pullPhoneBook(BluetoothPbapClient.PB_PATH);
             } catch (Exception e) {
                 e.printStackTrace();
             }
